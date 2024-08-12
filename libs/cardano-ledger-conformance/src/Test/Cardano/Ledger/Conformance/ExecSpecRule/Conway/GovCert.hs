1121ad0868d566593bca8bb8a2a5cba346b6d6a7
teodanciu <teodora.danciu@tweag.io>
2024-08-12 15:27:24 +0100

    Fixup agda result for GOVCERT rule to account for number of dormant epochs
    difference in `ConwayUpdateDRep`

--- libs/cardano-ledger-conformance/src/Test/Cardano/Ledger/Conformance/ExecSpecRule/Conway/GovCert.hs
+++ libs/cardano-ledger-conformance/src/Test/Cardano/Ledger/Conformance/ExecSpecRule/Conway/GovCert.hs
@@ -1,22 +1,26 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
-module Test.Cardano.Ledger.Conformance.ExecSpecRule.Conway.GovCert (nameGovCert) where
+module Test.Cardano.Ledger.Conformance.ExecSpecRule.Conway.GovCert (
+  nameGovCert,
+  adjustGStateForDormantEpochs,
+) where
 
+import Cardano.Ledger.BaseTypes
+import Cardano.Ledger.CertState (VState (..))
 import Cardano.Ledger.Conway
 import Cardano.Ledger.Conway.TxCert
+import Cardano.Ledger.Core
 import Data.Bifunctor (Bifunctor (..))
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Text as T
@@ -40,6 +44,31 @@ instance IsConwayUniv fn => ExecSpecRule fn "GOVCERT" Conway where
     first (\e -> OpaqueErrorString (T.unpack e) NE.:| [])
       . computationResultToEither
       $ Agda.govCertStep' env st sig
+
+  fixupAgdaResult ctx _ st sig =
+    bimap (fixup <$>) (adjustGStateForDormantEpochs @fn @"GOVCERT" ctx st sig . fixup)
+
+adjustGStateForDormantEpochs ::
+  ExecContext fn rule era ->
+  VState era ->
+  ConwayGovCert (EraCrypto era) ->
+  Agda.GState' ->
+  Agda.GState'
+adjustGStateForDormantEpochs
+  ctx
+  (VState {vsNumDormantEpochs})
+  cert
+  agdaSt@(Agda.MkGState' (Agda.MkHSMap ls) hks ds) =
+    case cert of
+      ConwayUpdateDRep cred _ ->
+        let expectRight' (Right x) = x
+            expectRight' (Left e) =
+              error ("Failed to translate cred: " <> show cred <> ", " <> show e)
+            agdaCred = expectRight' $ runSpecTransM ctx $ toSpecRep cred
+            updateEpoch e = toInteger $ fromInteger e - unEpochNo vsNumDormantEpochs
+            dreps = [(c, if agdaCred == c then updateEpoch e else e) | (c, e) <- ls]
+         in Agda.MkGState' (Agda.MkHSMap dreps) hks ds
+      _ -> agdaSt
 
 nameGovCert :: ConwayGovCert c -> String
 nameGovCert (ConwayRegDRep {}) = "ConwayRegDRep"
